ec13f93166ca0a7b2d452c39aad4e06abd798bfc
Greg Van Dell <gvandell@engineation.com>
2022-11-14 19:54:39 -0500

    using this repo again for frontend

--- components/GetLotteryToken.js
+++ components/GetLotteryToken.js
@@ -1,23 +1,69 @@
-import { useWeb3Contract } from "react-moralis"
+import { useMoralis, useWeb3Contract } from "react-moralis"
 import abi from "../constants/abi.json"
-import { useRef } from 'react'
+import { useRef, useEffect, useState } from 'react'
 
+const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+// tester address "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199"
 export default function GetLotteryToken() {
-    const inputEl = useRef(0);
+    const {isWeb3Enabled} = useMoralis()
+    const inputTokenAmount = useRef(0);
+    const inputNewCharityAddress = useRef(0);
+    const[charityAddress, setCharityAddressLocal] = useState("0")
+
+    const { runContractFunction: buyTicket } = useWeb3Contract ({
+        abi: abi,
+        contractAddress: CONTRACT_ADDRESS,
+        functionName: "buyTicket",
+        params: {tokenAmount: inputTokenAmount.current.value},
+        msgValue: "0",
+    })
+
+    const { runContractFunction: getCharityAddress } = useWeb3Contract ({
+        abi: abi,
+        contractAddress: CONTRACT_ADDRESS,
+        functionName: "getCharityAddress",
+        params: {},
+    })
+
+    const { runContractFunction: setCharityAddress } = useWeb3Contract ({
+        abi: abi,
+        contractAddress: CONTRACT_ADDRESS,
+        functionName: "setCharityAddress",
+        params: {_address: inputNewCharityAddress.current.value},
+    })
 
     const { runContractFunction: Tester } = useWeb3Contract ({
         abi: abi,
-        contractAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+        contractAddress: CONTRACT_ADDRESS,
         functionName: "Tester",
-        params: {testint: inputEl.current.value},
+        params: {testint: "100"},
     })
+
+    useEffect(() => {
+        async function updateUI() {
+            const charityAddressFromCall = await getCharityAddress()
+            setCharityAddressLocal(charityAddressFromCall)
+        }
+        if (isWeb3Enabled) {
+            updateUI()
+        }
+    }, [isWeb3Enabled])
 
     return(
         <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="Testlabel">Token Amount:</label>
-            <input ref={inputEl} type="text" id="tokenAmount" name="tokenAmount" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
-                onClick={Tester}>Get Lottery Token</button>
+                onClick={Tester}>Run Tester</button>
+
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="Testlabel">Token Amount:</label>
+            <input ref={inputTokenAmount} type="text" id="tokenAmount" name="tokenAmount" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
+                onClick={buyTicket}>Get Lottery Token</button>
+
+            <div>Last Charity Address: {charityAddress}</div>
+
+            <input ref={inputNewCharityAddress} type="text" id="newCharityAddress" name="newCharityAddress" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
+                onClick={setCharityAddress}>Set New Charity Address</button>
         </div>
     )
 }
--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import { ConnectButton } from "web3uikit"
 export default function Header() {
     return (
         <nav className="p-5 border-b-2 flex flex-row">
-            <h1 className="py-4 px-4 font-bold text-3xl">Blotto!</h1>
+            <h1 className="py-4 px-4 font-bold text-3xl">Blotto! The Blockchain Lottery DAO</h1>
             <div className="m1-auto py-2 px-4">
                 <ConnectButton moralisAuth={false} />
             </div>
